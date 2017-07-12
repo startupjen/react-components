6eb7ebad481463e137b20fa3c997f4d066b6a420
startupjen <jen@jentran.co>
2017-07-12 20:24:02 +0000

    Completed all sections

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+'use strict'
+
+// let Coffee = () => (
+//   <li>Coffee</li>
+// );
+
+// let Kale = () => (
+//   <li>Kale</li>
+// );
+
+
+// let GroceryList = () => (
+//   <ul id='groceryList'>
+//   <Coffee />
+//   <Kale />
+//   </ul>  
+// );
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      check: false
+    };
+  }
+  onListItemClick() {
+    this.setState({
+      check: !this.state.check
+    });
+  }
+  render() {
+    let style = { 'font-weight': this.state.check ? 'bold' : 'normal' };
+    return (<li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>);
+  }
+}
+
+let App = (props) => (
+  <ul>
+    {props.groceryItems.map( (item,index) => <GroceryListItem key={index} item={item} />) }
+  </ul>
+);
+
+let groceryItems = ['bloop','blah','whee'];
+
+ReactDOM.render(<App groceryItems={groceryItems} />, document.getElementById("app"));
+
